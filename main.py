d297ec96dcc5600fcd744190c79d2aaa60d3e41e
LuisParra00 <157584238+LuisParra00@users.noreply.github.com>
2024-11-17 10:53:09 +0000

    ddd

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ def main():
     ########################################
     """
 
+    while total <= 100:
+        num = random.randint(1, 10)
+        numbers.append(num)
+        total += num
+
+
+    total -= numbers[-1]
+
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
 
